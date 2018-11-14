14b878cb1647b29ac870d096904350821cf661bd
Trung Dao <trungdao912@gmail.com>
2018-11-14 20:21:46 +0700

    Session4-hw

--- Session4/app.js
+++ Session4/app.js
@@ -26,8 +26,27 @@ app.post("/ask", (req, res) => {
     res.redirect('/');
 })
 
+app.get('/randomquestion', (req, res) => {
+    
+    const randQuestion = JSON.parse(fs.readFileSync(path.join(__dirname, 'question.json'), 'utf-8'));
+    const randNum = Math.floor(Math.random() * randQuestion.length);
+    res.render('random', {data: randQuestion[randNum].content});
+})
+
+app.get('/question/:id', (req, res) => {
+    const id = req.params.id;
+    
+    const question = JSON.parse(fs.readFileSync(path.join(__dirname, 'question.json'), 'utf-8'));
+    if (id < question.length) {
+        res.render('id', {data: question[id].content});
+    }
+    else {
+        res.render('404');
+    }
+})
+
 app.get('/', (req, res) => {
-    res.send('Hello')
+    res.render('index');
 })
 
 app.listen(3000, (err) => {
